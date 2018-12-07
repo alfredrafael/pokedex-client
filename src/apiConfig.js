27379bc341481ac97e3a7601a1ab66e8c4e14286
zclark0625 <zclark0625@gmail.com>
2018-12-06 20:06:17 -0500

    Corrected URL Config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://zclark0625-pokedex.herokuapp.com/',
+  production: 'https://zclark0625-pokedex.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
